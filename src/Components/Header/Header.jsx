aa1d78f961377c7e985f6d7f35e298891aa75be2
AbujafarCh <357jafar@gmail.com>
2024-03-16 10:37:56 +0600

    all file uploading

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Yummeology</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Header;
